3669338fb7cdbc86bbdc5eb9769041a6bad40700
hu1149173393 <72081260+hu1149173393@users.noreply.github.com>
2021-02-21 20:10:08 +0800

    heihei

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,9 +9,22 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+    
+
+  },
+  created() {
+    let arr = [1,2,3,4,5,6,1,2,3]
+    arr.splice(0,3)
+    console.log(arr);
+  },
+  methods: {
+    one(){
+      
+    }
+  },
 }
 </script >
 
 <style lang="less">
 </style>
+186 6942 4516
