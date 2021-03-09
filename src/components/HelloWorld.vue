a7a6af466bedb47cd53bb4376fca8d82f5363bea
hu1149173393 <72081260+hu1149173393@users.noreply.github.com>
2021-03-09 21:44:52 +0800

    啦啦啦

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,14 +13,10 @@ export default {
 
   },
   created() {
-    let arr = [1,2,3,4,5,6,1,2,3]
-    arr.splice(0,3)
-    console.log(arr);
+
   },
   methods: {
-    one(){
-      
-    }
+
   },
 }
 </script >
